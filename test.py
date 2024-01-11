ba39aa32d4232025dd6305d218a476803cf42c43
Huz1012 <huzaifah1012i@gmail.com>
2024-01-11 21:28:52 +0000

    change 01

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ name ="huzaifah"
 print(name)
 print(name,"wants to make money.")
 print("To make money you have to have discipline.")
+print("Testing this small message to see if i can commit and push it to github")
